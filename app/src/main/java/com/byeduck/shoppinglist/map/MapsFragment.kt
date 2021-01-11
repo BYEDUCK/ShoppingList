9fadb0cb45218dbc3713d678df74af04705b9b83
Mateusz <s23667@pjwstk.edu.pl>
2021-01-11 21:49:17 +0100

    fix google maps camera zoom

--- app/src/main/java/com/byeduck/shoppinglist/map/MapsFragment.kt
+++ app/src/main/java/com/byeduck/shoppinglist/map/MapsFragment.kt
@@ -16,15 +16,6 @@ class MapsFragment(
 ) : Fragment() {
 
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
         shops.forEach {
             googleMap.addMarker(
                 MarkerOptions()
@@ -32,7 +23,12 @@ class MapsFragment(
                     .title(it.shopName)
             )
         }
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(shops[0].location)) // zoom to first marker
+
+        // zoom to first marker
+        // https://stackoverflow.com/questions/16458900/google-maps-api-v2-zooming-near-the-marker
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(shops[0].location, 15F))
+        googleMap.animateCamera(CameraUpdateFactory.zoomIn())
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(15F), 2000, null)
     }
 
     override fun onCreateView(
